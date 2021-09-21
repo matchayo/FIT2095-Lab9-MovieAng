8e781d6c75d42a81a2acd3e6f92b989bb725a2f8
Hafidza Hadi <hhad0002@student.monash.edu>
2021-09-22 01:38:54 +0700

    Remove MovieComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,11 @@ import { ActorComponent } from './actor/actor.component';
 import { DatabaseService } from './database.service';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ActorComponent,
-    MovieComponent
+    ActorComponent
   ],
   imports: [
     BrowserModule,
